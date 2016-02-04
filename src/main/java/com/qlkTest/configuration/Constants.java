1d4abd69bf9834f2d727d30d8f4a7566e5530646
昆布 <kunbu@7lk.com>
2016-02-04 15:27:02 +0800

    项目提交

---
+++ src/main/java/com/qlkTest/configuration/Constants.java
@@ -0,0 +1,12 @@
+package com.qlkTest.configuration;
+
+public class Constants {
+	//测试数据相关常亮设置
+	public static final String Path_ExcelFile="";
+	public static final String Path_ConfigurationFile="";
+	
+	//测试数据中的Sheet 中的列号常量设定
+	//第一列用0表示，测试用例序号列
+	public static final int Col_TestCaseID = 0;
+	
+}
